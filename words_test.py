413581566e38f18d54266c888b064f47d9861754
takkar <yurisuki915@gmail.com>
2022-08-27 16:16:05 +0900

    最後に不正解の単語を表示

--- words_test.py
+++ words_test.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # csvファイルの読み込み
-file = open("./testdata.csv", "r", encoding="utf-8", newline="")
+file = open("./BBC_words.csv", "r", encoding="utf-8", newline="")
 
 df = pd.read_csv(file, header=0, sep=",")
 
@@ -15,6 +15,7 @@ for i in range(len(df)):
 # テスト開始
 correctSum = 0
 questionSum = len(test_data)
+wrong_question = []  # 間違った単語を記録
 
 print("英単語テストを開始します\n")
 for key in test_data:
@@ -25,9 +26,19 @@ for key in test_data:
         correctSum += 1
     else:
         print("不正解！")
-
+        print("正解は:" + correctAnswer)
+        wrong_question.append([key, correctAnswer])
 
 # 結果を表示
 # 小数点第1位以下四捨五入
 correctPercentage = round((correctSum / questionSum) * 100, 1)
 print("テスト終了。正答率は" + str(correctPercentage) + "%です！")
+print("以下は不正解だった単語です")
+
+
+for k in range(len(wrong_question)):
+    for l in range(len(wrong_question[k])):
+        if l == 0:
+            print("単語:" + wrong_question[k][l])
+        elif l == 1:
+            print("意味:" + wrong_question[k][l])
